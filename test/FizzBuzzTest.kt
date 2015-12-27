ee94d2a6791f598d596cb4d59c15fe66a9710340
Jason <jason.vangundy@gmail.com>
2015-12-27 17:04:36 -0600

    refactor out dup class inits

--- test/FizzBuzzTest.kt
+++ test/FizzBuzzTest.kt
@@ -1,28 +1,36 @@
+import org.junit.Before
 import org.junit.Test
 import kotlin.test.*
 
 public class FizzBuzzTest {
 
+    lateinit var classToTest: FizzBuzz
+
+    @Before
+    fun setUp() {
+        classToTest = FizzBuzz()
+    }
+
     @Test
     fun testReturnsFizzFor3() {
-        assertEquals("fizz", FizzBuzz().get(3))
+        assertEquals("fizz", classToTest.get(3))
     }
 
     @Test
     fun testReturnsBuzzFor5() {
-        assertEquals("buzz", FizzBuzz().get(5))
+        assertEquals("buzz", classToTest.get(5))
     }
 
     @Test
     fun testReturnsNumberAsStringIfNotDivisibleBy5Or3() {
-        assertEquals("1", FizzBuzz().get(1))
+        assertEquals("1", classToTest.get(1))
     }
 
     @Test
     fun testReturnsFizzForMultiplesOf3() {
-        assertEquals("fizz", FizzBuzz().get(6))
-        assertEquals("fizz", FizzBuzz().get(9))
-        assertEquals("fizz", FizzBuzz().get(12))
+        assertEquals("fizz", classToTest.get(6))
+        assertEquals("fizz", classToTest.get(9))
+        assertEquals("fizz", classToTest.get(12))
     }
 }
 
